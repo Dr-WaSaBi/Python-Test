dfc68f6d50b52ba4dfb953b5513d26a68c4253ba
Russell <russellriker@gmail.com>
2018-01-28 15:16:24 -0500

    Py 3 changes
    Needed to make changes so code was Python 3 compliant

--- battleship.py
+++ battleship.py
@@ -12,10 +12,9 @@ for x in range(0, 5):
   board.append(["O"] * 5)
 
 def print_board(board):
-    ''' print_board funtion '''
-    
-  for row in board:
-    print " ".join(row)
+    ''' print_board fucntion '''
+    for row in board:
+        print(" ".join(row))
 
 print_board(board)
 
@@ -27,12 +26,12 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-guess_row = int(raw_input("Guess Row: "))
-guess_col = int(raw_input("Guess Col: "))
+guess_row = int(input("Guess Row: "))
+guess_col = int(input("Guess Col: "))
 
-print ship_row
-print ship_col
+print(ship_row)
+print(ship_col)
 
 # Write your code below!
 if guess_row == ship_row and guess_col == ship_col:
-  print "Congratulations! You sank my battleship!"
+  print("Congratulations! You sank my battleship!")
